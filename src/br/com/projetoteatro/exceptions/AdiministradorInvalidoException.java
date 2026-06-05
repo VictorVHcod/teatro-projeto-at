966b9738c63c8f8a04a5f3029cf8cfd53924dbc7
RayssHa <rayssafhardman@gmail.com>
2026-06-04 22:39:12 -0300

    adicionando exceptions

---
+++ src/br/com/projetoteatro/exceptions/AdiministradorInvalidoException.java
@@ -0,0 +1,7 @@
+package br.com.projetoteatro.exceptions;
+
+public class AdiministradorInvalidoException extends RuntimeException {
+    public AdiministradorInvalidoException(String message) {
+        super(message);
+    }
+}
